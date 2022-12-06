facb5e15c4c3fac58b8803e3aa7108bdba6f58c7
Jean-Charles ALLA <charlister22@mail.com>
2022-12-06 11:15:23 +0100

    log transformation done to 75%

--- WithLoggingUsingSpoon/src/main/java/main/Application.java
+++ WithLoggingUsingSpoon/src/main/java/main/Application.java
@@ -1,4 +1,41 @@
 package main;
 
+import processor.ThrowProcessor;
+import spoon.Launcher;
+import spoon.compiler.Environment;
+
+import java.util.Scanner;
+
 public class Application {
+    private Scanner sc;
+
+    public Application() {
+        this.sc = new Scanner(System.in);
+    }
+
+    private void transformMavenProject() {
+        System.out.print("Veuillez indiquer le repertoire vers le projet à analyser : ");
+        String projectPath = sc.nextLine().trim();
+
+        Launcher launcher = new Launcher();
+        launcher.addInputResource(projectPath);
+
+        Environment environment = launcher.getEnvironment();
+        environment.setNoClasspath(true);
+        environment.setComplianceLevel(11);
+        environment.setCommentEnabled(true);
+        environment.setAutoImports(true);
+
+        launcher.addProcessor(new ThrowProcessor());
+
+        System.err.println("Start of transformation ...");
+        launcher.run();
+        System.err.println("... End of transformation.");
+    }
+
+    public static void main(String[] args) {
+        Application app = new Application();
+
+        app.transformMavenProject();
+    }
 }
--- WithLoggingUsingSpoon/src/main/java/processor/ThrowProcessor.java
+++ WithLoggingUsingSpoon/src/main/java/processor/ThrowProcessor.java
@@ -1,4 +1,4 @@
-package localspoon.processor;
+package processor;
 
 import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.CtCodeSnippetStatement;
@@ -23,46 +23,62 @@ public class ThrowProcessor extends AbstractProcessor<CtThrow> {
     public void process(CtThrow ctThrow) {
         CtClass ctClass = getClass(ctThrow);
 
-        // Attributes
-        final CtTypeReference<java.util.logging.Logger> loggerTypeRef = getFactory().Code().createCtTypeReference(java.util.logging.Logger.class);
-        final CtField<java.util.logging.Logger> loggerField = getFactory().Core().createField();
-        loggerField.addModifier(ModifierKind.PRIVATE);
-        loggerField.addModifier(ModifierKind.FINAL);
-        loggerField.setType(loggerTypeRef);
-        loggerField.setSimpleName("LOGGER");
+        if (ctClass.getField("LOGGER") == null && ctClass.getField("fileHandler") == null) {
+            // Attributes
+            final CtTypeReference<java.util.logging.Logger> loggerTypeRef = getFactory().Code().createCtTypeReference(java.util.logging.Logger.class);
+            final CtField<java.util.logging.Logger> loggerField = getFactory().Core().createField();
+            loggerField.addModifier(ModifierKind.PRIVATE);
+            loggerField.addModifier(ModifierKind.STATIC);
+            loggerField.addModifier(ModifierKind.FINAL);
+            loggerField.setType(loggerTypeRef);
+            loggerField.setSimpleName("LOGGER");
 
-        final CtTypeReference<java.util.logging.Handler> handlerTypeRef = getFactory().Code().createCtTypeReference(java.util.logging.Handler.class);
-        final CtField<java.util.logging.Handler> fileHandlerField = getFactory().Core().createField();
-        fileHandlerField.addModifier(ModifierKind.PRIVATE);
-        fileHandlerField.addModifier(ModifierKind.FINAL);
-        fileHandlerField.setType(handlerTypeRef);
-        fileHandlerField.setSimpleName("fileHandler");
+            final CtTypeReference<java.util.logging.Handler> handlerTypeRef = getFactory().Code().createCtTypeReference(java.util.logging.Handler.class);
+            final CtField<java.util.logging.Handler> fileHandlerField = getFactory().Core().createField();
+            fileHandlerField.addModifier(ModifierKind.PRIVATE);
+            fileHandlerField.addModifier(ModifierKind.FINAL);
+            fileHandlerField.setType(handlerTypeRef);
+            fileHandlerField.setSimpleName("fileHandler");
 
-        ctClass.addField(loggerField);
-        ctClass.addField(fileHandlerField);
+            ctClass.addFieldAtTop(fileHandlerField);
+            ctClass.addFieldAtTop(loggerField);
 
-        // Constructor
-        Set<CtConstructor> ctConstructors = ctClass.getConstructors();
-        String initLogger = "this.LOGGER = Logger.getLogger(this.getClass().getName();";
-        String tryCatchFileHandler = "try {\n" +
-                "            fileHandler  = new FileHandler(\"./traceability.log\");\n" +
-                "            LOGGER.addHandler(fileHandler);\n" +
-                "            fileHandler.setLevel(java.util.logging.Level.ALL);\n" +
-                "            LOGGER.setLevel(java.util.logging.Level.ALL);\n" +
-                "        } catch (java.io.IOException e) {\n" +
-                "            LOGGER.log(java.util.logging.Level.INFO, \"Error occur in FileHandler.\", e);\n" +
-                "        }";
-        for (CtConstructor ctConstructor : ctConstructors) {
-            ctConstructor.getBody().addStatement(this.getFactory().Code().createCodeSnippetStatement(initLogger));
-            ctConstructor.getBody().addStatement(this.getFactory().Code().createCodeSnippetStatement(tryCatchFileHandler));
+            // Constructor
+            Set<CtConstructor> ctConstructors = ctClass.getConstructors();
+            String initLogger = String.format("this.LOGGER = java.util.logging.Logger.getLogger(%s.class.getName());", ctClass.getSimpleName());
+            String tryCatchFileHandler = "try {\n" +
+                    "   fileHandler  = new java.util.logging.FileHandler(\"./traceability.log\");\n" +
+                    "   LOGGER.addHandler(fileHandler);\n" +
+                    "   fileHandler.setLevel(java.util.logging.Level.ALL);\n" +
+                    "   LOGGER.setLevel(java.util.logging.Level.ALL);\n" +
+                    "} catch (java.io.IOException e) {\n" +
+                    "   LOGGER.log(java.util.logging.Level.SEVERE, \"Error occur in FileHandler.\", e);\n" +
+                    "}";
+            for (CtConstructor ctConstructor : ctConstructors) {
+                ctConstructor.getBody().addStatement(this.getFactory().Code().createCodeSnippetStatement(initLogger));
+                ctConstructor.getBody().addStatement(this.getFactory().Code().createCodeSnippetStatement(tryCatchFileHandler));
+            }
         }
 
         // instruction throw
-        CtCodeSnippetStatement logMsgStatement = this.getFactory().Code().createCodeSnippetStatement("LOGGER.log(Level.WARNING, )");
+        String throwStatementStr = "";
+        throwStatementStr = String.valueOf(ctThrow.getThrownExpression());
+        CtCodeSnippetStatement logMsgStatement = this.getFactory().Code().createCodeSnippetStatement(String.format("LOGGER.log(Level.INFO, %s)", extractMessageFromThrowStatement(throwStatementStr)));
         ctThrow.insertBefore(logMsgStatement);
         ctThrow.comment();
 
         // gérer la valeur retournée par la méthode
+    }
+
+    private String extractMessageFromThrowStatement(String throwStatementStr) {
+        String result = "";
+
+        int msgBeginIndex = throwStatementStr.indexOf("(")+1;
+        int msgEndIndex = throwStatementStr.lastIndexOf(")");
+
+        result = throwStatementStr.substring(msgBeginIndex, msgEndIndex);
+
+        return result;
     }
 
     private CtClass getClass(CtThrow ctThrow) {
